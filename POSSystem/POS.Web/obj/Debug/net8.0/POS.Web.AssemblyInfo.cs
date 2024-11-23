78a9bd622967ff2ddbcd09c984e9ad03d28f8ebe
Weerawat-rgb <weerawat.nart@outlook.com>
2024-11-23 20:32:55 +0700

    commit

--- POSSystem/POS.Web/obj/Debug/net8.0/POS.Web.AssemblyInfo.cs
+++ POSSystem/POS.Web/obj/Debug/net8.0/POS.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POS.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78eca84de2156d184161b118fe38e9cb8e65571e")]
 [assembly: System.Reflection.AssemblyProductAttribute("POS.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POS.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
